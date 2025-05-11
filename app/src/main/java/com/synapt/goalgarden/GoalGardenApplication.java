2b00a77fdea17bfa32170bbd5e04b78443ee87c0
IJssel <ijssel@synapt.nl>
2025-05-11 19:51:13 +0200

    Added spring structure

---
+++ app/src/main/java/com/synapt/goalgarden/GoalGardenApplication.java
@@ -0,0 +1,13 @@
+package com.synapt.goalgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalGardenApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/synapt/goalgarden/GoalGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synapt.goalgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
